e98540a28433c31ceb75238307a5bae48392ac55
trixo <matus.satara@gmail.com>
2019-01-22 14:09:19 +0100

    Update ShowComments.js

--- src/components/StreamerProfile/Comments/ShowComments/ShowComments.js
+++ src/components/StreamerProfile/Comments/ShowComments/ShowComments.js
@@ -3,7 +3,7 @@ import classes from "./ShowComments.module.css"
 import Comment from "./Comment/Comment"
 const showComments= (props) =>{
 console.log(props)
-let comments= newArr = [...props.Comments];
+let comments= [...props.Comments];
 comments=comments.reverse().map((comment,index)=><Comment key={index} Comment={comment}/>);
 	return(
 
