2687bfcc7e45b21c57e6c6111dd16b4aede83596
trixo <matus.satara@gmail.com>
2019-01-22 14:19:28 +0100

    Update ShowComments.js

--- src/components/StreamerProfile/Comments/ShowComments/ShowComments.js
+++ src/components/StreamerProfile/Comments/ShowComments/ShowComments.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classes from "./ShowComments.module.css"
 import Comment from "./Comment/Comment"
 const showComments= (props) =>{
-var comments = props.Comments.slice();
-comments=comments.reverse().map((comment,index)=><Comment key={index} Comment={comment}/>);
+
+let comments=props.Comments.reverse().map((comment,index)=><Comment key={index} Comment={comment}/>);
 	return(
 
 <div className={classes.ShowComments}>
